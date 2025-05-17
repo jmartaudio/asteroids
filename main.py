aff92bdf3d2009e3e9dc7409fcd26d4be29f8c3f
Jason Martinez <jason@bigpictureranch.com>
2025-05-17 14:49:43 -0700

    got a triagle on screen

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import pygame
-import constants 
-
 from constants import *
-
+from player import Player 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}") 
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))
+        screen.fill("black")
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()
-
-
-
-
-
-
-
-
-
-
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
