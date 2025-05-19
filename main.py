d16bbb5174261ef6550a5688ddbacc43eae3a9e3
Jason Martinez <jason@bigpictureranch.com>
2025-05-19 09:43:58 -0700

    fully working

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
-d
+                
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
